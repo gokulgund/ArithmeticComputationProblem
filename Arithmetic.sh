8209d3fb972d9d467056a32980dcbf263ef73edf
gokul gund <gokulgund15@gmail.com>
2021-11-02 11:56:53 +0530

    Added:Enter three number

---
+++ Arithmetic.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+read -p "Enter the First Number :  " a
+
+read -p "Enter the Second Number : " b
+
+
+read -p "Enter the Third Number : " c
+
+echo  "First Number is : $a"
+echo "second Number is : $b"
+echo "Third Number is : $c"
