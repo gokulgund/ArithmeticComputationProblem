7f045be9d1fc6801adab98f313cb5032ed44fdfc
gokul gund <gokulgund15@gmail.com>
2021-11-03 11:27:54 +0530

    Added: Compute Arithmatic Operation A*B+C

--- Arithmetic.sh
+++ Arithmetic.sh
@@ -1,12 +1,10 @@
 #!/bin/bash 
 
 read -p "Enter the First Number :  " a
-
 read -p "Enter the Second Number : " b
-
-
 read -p "Enter the Third Number : " c
-
-echo  "First Number is : $a"
-echo "second Number is : $b"
-echo "Third Number is : $c"
+echo  "First Number A is : $a"
+echo "second Number B is : $b"
+echo "Third Number C is : $c"
+s=$((a*b+c))
+echo "a*b+c="$s
